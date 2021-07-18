88611247e092f6118acbe2807aebae265f60e330
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-07-18 18:35:53 -0300

    chore: add firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: ""
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database }
